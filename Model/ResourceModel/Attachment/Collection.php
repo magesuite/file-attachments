2900c347c18e294b51c55467436b70a4fb805334
Blazej Doleska <blazej.doleska@creativestyle.pl>
2024-03-11 12:36:58 +0100

    removed not needed constructor override

--- Model/ResourceModel/Attachment/Collection.php
+++ Model/ResourceModel/Attachment/Collection.php
@@ -5,17 +5,6 @@ namespace MageSuite\FileAttachments\Model\ResourceModel\Attachment;
 
 class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
 {
-    public function __construct(
-        \Magento\Framework\Data\Collection\EntityFactoryInterface $entityFactory,
-        \Psr\Log\LoggerInterface $logger,
-        \Magento\Framework\Data\Collection\Db\FetchStrategyInterface $fetchStrategy,
-        \Magento\Framework\Event\ManagerInterface $eventManager,
-        \Magento\Framework\DB\Adapter\AdapterInterface $connection = null,
-        \Magento\Framework\Model\ResourceModel\Db\AbstractDb $resource = null
-    ) {
-        parent::__construct($entityFactory, $logger, $fetchStrategy, $eventManager, $connection, $resource);
-    }
-
     protected function _construct(): void
     {
         $this->_init(\MageSuite\FileAttachments\Model\Attachment::class, \MageSuite\FileAttachments\Model\ResourceModel\Attachment::class);
